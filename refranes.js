e2f5629225c26b2fafc9cc9f3840a6999ea75bb8
Miquel Angel Bardaji <mikibardaji@hotmail.com>
2024-12-04 09:04:57 +0100

    Add files via upload

--- refranes.js
+++ refranes.js
@@ -29,3 +29,28 @@ function siguienteRefran() {
     cargarRefran();
     document.getElementById('finRefran').style.display = 'none';
 }
+
+
+function irAlRefran() {
+    const inputNumero = document.getElementById('numeroRefran').value;
+    const numero = parseInt(inputNumero, 10);
+
+    if (!isNaN(numero) && numero > 0 && numero <= refranes.length) {
+        indiceActual = numero - 1; // Els refranys comencen en 0 al array.
+        cargarRefran();
+    } else {
+        alert('Por favor, introduce un número válido entre 1 y ' + refranes.length);
+    }
+}function cargarRefran() {
+    if (refranes.length > 0) {
+        const refran = refranes[indiceActual];
+        document.getElementById('inicioRefran').textContent = refran.inicio;
+        document.getElementById('finRefran').textContent = refran.fin;
+        document.getElementById('finRefran').style.display = 'none';
+
+        // Actualitzar el número del refrany actual.
+        document.getElementById('numeroActual').textContent = `Refrán actual: ${indiceActual + 1}`;
+    }
+}
+
+
